f2cafab4a19aa62647df5814d4a7495d0ca9e8d4
Leander Molegraaf <leander@nedbase.nl>
2017-03-15 17:13:21 +0100

    added frontend back, with some css/js files converted for gulp-ification, and some replaced for a CDN. Also added initial support for sending emails

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
-const elixer = require('laravel-elixir');
+const elixir = require('laravel-elixir');
 
-elixer(function(mix) {
-    mix.sass(["_variables.scss", "app.scss", "black.scss", "mywall.scss"])
-        .scripts([""]);
+elixir(function(mix) {
+    mix.sass([""]);
 });
